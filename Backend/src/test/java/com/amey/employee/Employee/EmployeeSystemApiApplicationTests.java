fecc04e356b7d3af5e9849be74ea3b9ce37fb2ce
Amey <bamey2241997@gmail.com>
2022-12-15 09:45:47 +0530

    Added Employee Management System

---
+++ Backend/src/test/java/com/amey/employee/Employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amey.employee.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
